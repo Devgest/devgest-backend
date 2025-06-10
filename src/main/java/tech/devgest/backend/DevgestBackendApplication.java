38efef8beba397d32af8790ba7d7bfdcd44c1f1b
AnuragPandey01 <anuragpandey87187@gmail.com>
2025-06-10 20:25:00 +0530

    initial commit

---
+++ src/main/java/tech/devgest/backend/DevgestBackendApplication.java
@@ -0,0 +1,13 @@
+package tech.devgest.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevgestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevgestBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/devgest/backend/DevgestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.devgest.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevgestBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
